b6e11a906338ccaebc700d68a5e05e1a6190d608
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-06-24 11:45:26 -1000

    add aws s3

--- models/pet.js
+++ models/pet.js
@@ -13,8 +13,9 @@ const PetSchema = new Schema({
   name: { type: String, required: true }
   , birthday: {type: String, required: true }
   , species: { type: String, required: true }
-  , picUrl: { type: String, required: true }
-  , picUrlSq: { type: String, required: true }
+  , picUrl: { type: String, required: false }
+  , picUrlSq: { type: String, required: false }
+  , avatarUrl: { type: String, required: false }
   , favoriteFood: { type: String, required: true }
   , description: { type: String, minlength: 140, required: true }
 }, {
